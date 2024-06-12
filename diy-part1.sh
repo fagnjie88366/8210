a7789f195982d0988f0298dcf412f20748104a01
fagnjie88366 <73266244+fagnjie88366@users.noreply.github.com>
2024-06-12 11:26:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci-app-ikoolproxy https://github.com/fagnjie88366/luci-app-ikoolproxy' >>feeds.conf.default
-echo 'src-git luci-app-oaf https://github.com/fagnjie88366/OpenAppFilter' >>feeds.conf.default
